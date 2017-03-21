3c2128c133beac0556c0f328b5441814497f5ec0
Filip <palka.fil@gmail.com>
2017-03-21 22:17:11 +0100

    Project in Maven

---
+++ src/main/java/virtualDeanery/controller/HomeController.java
@@ -0,0 +1,18 @@
+package virtualDeanery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+	@RequestMapping("/")
+	public String welcome(Model model)
+	{
+		model.addAttribute("witaj", "witaj na naszej stronie");
+		
+		return "index";
+	}
+
+}
