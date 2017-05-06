db5852655e25342323f8c7b62a8c4ff15c492b94
MateuszGładysz <mateuszg1305@gmial.com>
2017-05-06 15:19:49 +0200

    added editAccount

--- src/main/java/virtualDeanery/service/UserService.java
+++ src/main/java/virtualDeanery/service/UserService.java
@@ -5,12 +5,20 @@ import java.util.List;
 import virtualDeanery.model.User;
 import virtualDeanery.model.User_Account;
 
-public interface UserService
-{
-	List <User> getAllUsers();
+public interface UserService {
+	List<User> getAllUsers();
+
 	User getUserByNiu(int niu);
+
 	User loginUser(int niu, String password);
+
 	User loginUserTest(int niu, String imie);
+
 	User_Account getUser_AccountByNiu(int niu);
-	
+
+	public void changeContactDetails(String newPhone, String newEmail, String newAddress, String newCity,
+			String newPost_code, User user);
+
+	public String changePassword(String currentPassword, String newPassword1, String newPassword2, User user);
+
 }
--- src/main/java/virtualDeanery/service/impl/UserServiceImpl.java
+++ src/main/java/virtualDeanery/service/impl/UserServiceImpl.java
@@ -12,26 +12,22 @@ import virtualDeanery.model.repository.User_AccountRepository;
 import virtualDeanery.service.UserService;
 
 @Service
-public class UserServiceImpl implements UserService
-{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
 	@Autowired
 	User_AccountRepository user_accRepository;
 
-	public List<User> getAllUsers()
-	{
+	public List<User> getAllUsers() {
 		return userRepository.getAllUsers();
 	}
 
-	public User getUserByNiu(int niu)
-	{
+	public User getUserByNiu(int niu) {
 		return userRepository.getUserByNiu(niu);
 	}
 
-	public User loginUserTest(int niu, String imie)
-	{
+	public User loginUserTest(int niu, String imie) {
 
 		User user = userRepository.getUserByNiu(niu);
 		System.out.println("Imie w parametrze " + imie);
@@ -42,14 +38,12 @@ public class UserServiceImpl implements UserService
 		return null;
 	}
 
-	public User loginUser(int niu, String password)
-	{
-		System.out.println("Niu: "+niu+"\nPass: "+password);
+	public User loginUser(int niu, String password) {
+		System.out.println("Niu: " + niu + "\nPass: " + password);
 		User_Account user_acc = user_accRepository.getUser_AccountByNiu(niu);
-		
+
 		System.out.println(user_acc);
-		if (user_acc != null && user_acc.getPassword().equals(password))
-		{
+		if (user_acc != null && user_acc.getPassword().equals(password)) {
 			User user = userRepository.getUserByNiu(niu);
 
 			return user;
@@ -58,8 +52,58 @@ public class UserServiceImpl implements UserService
 		return null;
 	}
 
-	public User_Account getUser_AccountByNiu(int niu)
-	{
+	public void changeContactDetails(String newPhone, String newEmail, String newAddress, String newCity,
+			String newPost_code, User user) {
+          
+		if(!newPhone.equals("")){
+			user.setPhone(newPhone);
+		}
+		if(!newEmail.equals("")){
+			user.setEmail(newEmail);
+		}
+		if(!newAddress.equals("")){
+			user.setAddress(newAddress);
+		}
+		if(!newCity.equals("")){
+			user.setCity(newCity);
+		}
+		if(!newPost_code.equals("")){
+			user.setPost_code(newPost_code);
+		}       
+	      userRepository.updateUser(user);
+	}
+	
+	public String changePassword(String currentPassword, String newPassword1, String newPassword2, User user){
+		
+		User_Account user_acc = user_accRepository.getUser_AccountByNiu(user.getId());
+		
+		if(currentPassword.equals(user_acc.getPassword())){
+			
+			if(newPassword1.equals(newPassword2)){
+				if(newPassword1.length()>5){
+					
+					user_acc.setPassword(newPassword1);
+					user_accRepository.changePasssword(user_acc);
+				}
+				else{
+					System.out.println("Has這 zakr鏒kie");
+					return "Has這 za kr鏒kie";
+				}
+			}
+			else{
+				System.out.println("B貫dne powt鏎zenie nowego has豉");
+				return "B貫dne powt鏎zenie nowego has豉";
+			}
+		}
+		else{
+			System.out.println("Obecne has這 jest nie prawid這we");
+			return "Obecne has這 jest nie prawid這we";
+		}
+		System.out.println("Has這 zmienione");
+		return "Has這 zosta這 zmienione";
+	}
+
+	public User_Account getUser_AccountByNiu(int niu) {
 		return user_accRepository.getUser_AccountByNiu(niu);
 	}
 
