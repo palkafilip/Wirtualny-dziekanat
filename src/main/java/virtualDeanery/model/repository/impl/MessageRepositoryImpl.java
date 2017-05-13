d8bc13f383d4b839ebd2f2601cb5257361d8e513
czapiewskimateusz <matis1enator@gmail.com>
2017-05-13 18:02:31 +0200

    Added message

---
+++ src/main/java/virtualDeanery/model/repository/impl/MessageRepositoryImpl.java
@@ -0,0 +1,59 @@
+package virtualDeanery.model.repository.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import virtualDeanery.model.Message;
+import virtualDeanery.model.Msg_recipient;
+import virtualDeanery.model.repository.MessageRepository;
+
+@Repository
+public class MessageRepositoryImpl implements MessageRepository {
+
+	@Autowired
+	DataSource datasource;
+
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	public MessageRepositoryImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public List<Message> getAllMessagesByNiu(int niu, List<Msg_recipient> msgGetRecipientList) {
+
+		//Lista przechowuj¹ca msg_id wiadomoœci dla zalogowanego u¿ytkownika
+		ArrayList<Integer> msgRecipientList = new ArrayList<Integer>();
+
+		/**
+		 * Pobieramy tylko msg_id z klasy Msg_recipient
+		 */
+		for (int i = 0; i < msgGetRecipientList.size(); i++) {
+			msgRecipientList.add(msgGetRecipientList.get(i).getMsg_id());
+		}
+
+	
+		//Lista przechowuj¹ca wiadomoœci u¿ytkownika
+		List<Message> listMessage = new ArrayList<Message>();
+		
+		/**
+		 * Pobieramy wszystkie wiadomoœci dla zalogowanego u¿ytkownika
+		 */
+		for (int i = 0; i < msgRecipientList.size(); i++) {
+			listMessage.add((Message) sessionFactory.getCurrentSession().get(Message.class,msgRecipientList.get(i)));
+		}
+		
+		
+		return listMessage;
+	}
+
+}
---
+++ src/main/java/virtualDeanery/service/MessageService.java
@@ -0,0 +1,11 @@
+package virtualDeanery.service;
+
+import java.util.List;
+
+import virtualDeanery.model.Message;
+
+public interface MessageService {
+	
+	List<Message> getAllMessages(int niu);
+
+}
