cc07ff90307f9d1a23d6c9578bfa15fa8c264aca
czapiewskimateusz <matis1enator@gmail.com>
2017-05-27 17:36:33 +0200

    Applications added

--- src/main/java/virtualDeanery/controller/ApplicationController.java
+++ src/main/java/virtualDeanery/controller/ApplicationController.java
@@ -1,15 +1,52 @@
 package virtualDeanery.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import virtualDeanery.model.User;
 
 @Controller
-public class ApplicationController
-{
+public class ApplicationController {
+	
+	private static String UPLOADED_FOLDER = "C://podania//";
+	
 	@RequestMapping("/application")
-	public String applicationController(Model model)
-	{
+	public String applicationController(Model model) {
 		return "application";
 	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, Model model, HttpSession session) {
+
+		User currentUser = (User) session.getAttribute("loggedInUser");
+		
+		try {
+			
+			byte[] bytes = file.getBytes();
+			
+			Path path = Paths.get(UPLOADED_FOLDER + currentUser.getId() + file.getOriginalFilename());
+			
+			Files.write(path, bytes);
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		model.addAttribute("message", "Wysy³anie pliku zakoñczone powodzeniem");
+		return "application";
+	}
+
 }
--- src/main/webapp/WEB-INF/views/application.jsp
+++ src/main/webapp/WEB-INF/views/application.jsp
@@ -1,11 +1,32 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <tiles:insertDefinition name="defaultTemplate">
     <tiles:putAttribute name="body">
     
     <h1>Podania</h1>
-        
+    
+    <div class="form-group">
+    <a href="<c:url value='resources/applications/dkEnergetyka.doc' /> " > Dług kredytowy Energetyka:<img src="<c:url value='resources/icons/doc_icon.png' /> "style= "width:50px;height:50px;"></a>
+    </div>
+    <div class="form-group">
+    <a href="<c:url value='resources/applications/dkInformatyka.doc' /> " > Dług kredytowy Informatyka:<img src="<c:url value='resources/icons/doc_icon.png' /> "style= "width:50px;height:50px;"></a>
+    </div>
+    <div class="form-group">
+    <a href="<c:url value='resources/applications/przepisanie.doc' /> " > Przepisanie oceny:<img src="<c:url value='resources/icons/doc_icon.png' /> "style= "width:50px;height:50px;"></a>
+    </div>
+    
+    
+    <h1>Wstaw podanie</h1>
+    <form method="POST" action="upload" enctype="multipart/form-data">
+    <input type="file" name="file" /><br/>
+    <input type="submit" value="Wyślij" />
+</form>
+    
+    <p> ${message}</p>
+    
     </tiles:putAttribute>
 </tiles:insertDefinition>
